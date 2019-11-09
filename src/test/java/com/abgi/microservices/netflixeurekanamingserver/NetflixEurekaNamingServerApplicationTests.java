0d7582cf8aa1394872aa724749bbff16155bba1f
abimaelrsergio <abimaelr.sergio@gmail.com>
2019-11-09 00:57:11 -0300

    first commit

---
+++ src/test/java/com/abgi/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abgi.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
